99ac9e3142453a42137ad8c736947901657fc55f
avihavai <avihavai@XX.helsinki.fi>
2015-09-13 11:20:17 +0300

    added tracing of users and a scoreboard

--- js/wepa.js
+++ js/wepa.js
@@ -93,8 +93,119 @@ var SITE = {
     }
 };
 
+var auth = {
+    username: "",
+    password: "",
+    authed: false,
+    tmc_url: "https://tmc.mooc.fi/#{h tmc_instance}/auth.json",
+    date_asked: new Date(),
+    init: function(){
+        auth.username = localStorage.getItem("tmc.account.name");
+        var maybeDate = localStorage.getItem("tmc.date_asked");
+        if ("null" != maybeDate){
+            auth.date_asked = maybeDate;
+        }
+    },
+    persistent: function(){
+        localStorage.setItem("tmc.account.name", auth.username);
+        localStorage.setItem("tmc.date_asked", auth.date_asked);
+    },
+    logout: function(){
+        localStorage.setItem("tmc.account.name", "");
+        location.reload();
+    },
+    authenticate: function(successFunc){
+        console.log("authing");
+        $('#tmcAuthModal').modal('show');
+	
+        $('#TmcLoginForm').submit(function(event) {
+            var tmcAccount = $('#inputTmcAccount').val();
+            auth.username = tmcAccount;
+            localStorage.setItem("tmc.account.name", auth.username);
+            auth.authed = true;
+            $('#tmcAuthModal').modal('hide');
+            successFunc();
+	    
+            return false;
+        });
+    }
+};
+
+function tmcAuth(callbackFunction){
+    var logoutButton = $('#logout');
+    logoutButton.hide();
+    auth.init();
+    var needToAskAuth = true;
+    // init db
+    console.log(JSON.stringify(auth));
+    var datee = Date.parse(auth.date_asked);
+    
+    if (undefined ==  datee){
+        datee = new Date();
+        auth.date_asked = datee;
+    }    
+    
+    var date1 = new Date();
+    date1.setDate(date1.getDate() - 2);
+    
+    if (null == localStorage.getItem("tmc.version")){
+        localStorage.setItem("tmc.version", "0");
+    }
+    if ( auth.username && auth.username.length > 1 && "null" != auth.username){
+        var deprecatedSettings = localStorage.getItem("tmc.version");
+        if ( deprecatedSettings && deprecatedSettings.length > 0 ){
+            deprecatedSettings = parseInt(deprecatedSettings) || 0;
+            if (deprecatedSettings >= 2){
+                needToAskAuth = false;
+            }
+        }
+    }
+    
+    localStorage.setItem("tmc.version", "2");
+    if (needToAskAuth){
+        auth.authenticate(function() {
+            callbackFunction();
+        });
+        needToAskAuth = false;
+    } else {
+        callbackFunction();
+    }
+        
+    $('#logout').click(function(){
+        auth.logout();
+        auth.authenticate();
+    });
+    
+    auth.persistent();
+}
+
+
 $(function() {
+    console.log("Init headers");
     SITE.init();
+    console.log("Init syntax highlighting");
     sh_highlightDocument('js/libs/syntaxhighlight/lang/', '.min.js');
+
+    tmcAuth(function() {
+	if(!auth.username) {
+	    return;
+	}
+
+	$('#logout').show();
+        $('#logout').append(": " + auth.username);
+	
+	var storedUsername = localStorage.getItem("tmc.account.name");
+	logger.setUser(storedUsername);
+	logger.setApiUrl("http://data.pheromones.io/")
+	logger.init();
+	
+	pheromones.init({
+            apiUrl: "http://data.pheromones.io/",
+            username: storedUsername,
+            submitAfter: 20
+	});
+	
+	VERTICALFLOAT.init(storedUsername);
+    });
 });
 
