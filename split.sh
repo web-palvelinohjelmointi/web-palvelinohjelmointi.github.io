4fd1bd27c0d178546e137fa7918381362e9affd6
avihavai <avihavai@x1c-9.cs.helsinki.fi>
2016-07-11 01:35:42 +0300

    w1

--- split.sh
+++ split.sh
@@ -1,13 +1,18 @@
 if [ -f "index.html" ]
 then
     rm index.html
+    rm osa1.html
 fi
 
 INDEX=0
 HEADER=0
 NAV=0
+CONTENT=0
 FOOTER=0
+OSA1=0
 
+
+IFS=''
 while read line; do
     case "$line" in
         *BEGIN*HEADER*) 
@@ -28,16 +33,31 @@ while read line; do
         *END*NAV*)
             NAV=0
             ;;
+        *BEGIN*CONTENT*)
+            CONTENT=1
+            ;;
+        *END*CONTENT*)
+            CONTENT=0
+            ;;
         *BEGIN*INDEX*)
             INDEX=1
             ;;
         *END*INDEX*)
             INDEX=0
             ;;
+        *BEGIN*OSA1*)
+            OSA1=1
+            ;;
+        *END*OSA1*)
+            OSA1=0
+            ;;
     esac
 
     if [ $HEADER -eq 1 ] || [ $FOOTER -eq 1 ] || [ $INDEX -eq 1 ]; then
         echo $line >> index.html
+    fi
+    if [ $HEADER -eq 1 ] || [ $FOOTER -eq 1 ] || [ $OSA1 -eq 1 ] || [ $CONTENT -eq 1 ]; then
+        echo $line >> osa1.html
     fi
 
 done < 2016-mooc.html
